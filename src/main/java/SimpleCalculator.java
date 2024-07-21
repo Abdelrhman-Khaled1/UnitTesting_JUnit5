4eaf2800c8aa189868b67b805a98354063f33f35
Abdelrhman <kabdo17013@gmail.com>
2024-07-21 13:47:47 +0300

    Applying assertAll and using lambda to perform better

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -2,8 +2,11 @@ public class SimpleCalculator {
     public int add(int numberA, int numberB) {
         return numberA + numberB;
     }
-    public int divide(int a , int b){
-        return a/b;
+    public int divide(int number1 , int number2){
+        return number1/number2;
     }
 
+    public int multiply(int number1 , int number2){
+        return number1 * number2;
+    }
 }
--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -6,15 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleCalculatorTest {
 
-    @Test
-    void twoPlusTwoShouldEqualFour(){
-        var calculator = new SimpleCalculator();
-        assertEquals (4,calculator.add(2,2));
+    SimpleCalculator calculator;
+
+    @BeforeEach
+    void init() {
+        calculator = new SimpleCalculator();
     }
+
     @Test
-    void threePlusSevenShouldEqualTen(){
+    void twoPlusTwoShouldEqualFour() {
         var calculator = new SimpleCalculator();
-        assertEquals (10,calculator.add(3,7));
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    void threePlusSevenShouldEqualTen() {
+        var calculator = new SimpleCalculator();
+        assertEquals(10, calculator.add(3, 7));
     }
 
     @Test
@@ -36,6 +46,26 @@ class SimpleCalculatorTest {
     @ValueSource(ints = {2, 4, 6, 8})
     public void testEvenNumbers(int number) {
         assertTrue(number % 2 == 0); // Verifying that all numbers in the list are even
+    }
+
+    @Test
+    @DisplayName("multiply method")
+    void testMultiply() {
+        assertAll(
+                () -> assertEquals(4, calculator.multiply(2, 2)),
+                () -> assertEquals(0, calculator.multiply(2, 0)),
+                () -> assertEquals(-2, calculator.multiply(2, -1))
+        );
+    }
+
+    @Test
+    @DisplayName("When adding two negative numbers")
+    void testAddNegative() {
+        int expected = -2;
+        int actual = calculator.add(-1, -1);
+        //very expensive operation assertEquals take 3 parameters, and we compute the string even we don't need it
+        //assertEquals(expected, actual, "Should return sum " + expected + " but returned " + actual);
+        assertEquals(expected, actual, () -> "Should return sum " + expected + " but returned " + actual); // now it was a lambda and will be called only if the function fail! Wow
     }
 
 }
